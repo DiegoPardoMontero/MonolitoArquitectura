4f5931c053ab72fa4bb10cfac713fb3cc52a3836
DiegoPardoMontero <di-pardo@javeriana.edu.co>
2024-09-23 12:01:58 -0500

    :lipstick: Change video format

--- app/src/main/java/com/puj/proyectoensenarte/util/CustomVideoView.kt
+++ app/src/main/java/com/puj/proyectoensenarte/util/CustomVideoView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.net.Uri
 import android.util.AttributeSet
 import android.view.Gravity
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
@@ -23,42 +22,41 @@ class CustomVideoView @JvmOverloads constructor(
     private val controllerContainer: FrameLayout
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.  custom_video_view, this, true)
-
+        inflate(context, R.layout.custom_video_view, this)
         videoView = findViewById(R.id.videoView)
         controllerContainer = findViewById(R.id.controllerContainer)
 
         mediaController = object : MediaController(context) {
-            override fun show() {
-                super.show(0)
+            override fun show(timeout: Int) {
+                super.show(timeout)
+                visibility = View.VISIBLE
             }
 
             override fun hide() {
-                // No hacer nada, mantener siempre visible
+                super.hide()
+                visibility = View.GONE
             }
         }
-
-        // No establecer el anchorView aquí
-        // mediaController.setAnchorView(controllerContainer)
     }
 
     fun setupVideo(url: String?) {
         if (url != null) {
             videoView.setVideoURI(Uri.parse(url))
             videoView.setMediaController(mediaController)
-            mediaController.setAnchorView(controllerContainer) // Mover aquí
+            mediaController.setAnchorView(controllerContainer)
+
             videoView.setOnPreparedListener { mp ->
                 mp.isLooping = true
-                videoView.pause()
-                videoView.seekTo(1)
                 addMediaControllerToContainer()
+                mediaController.show(0)  // Mostrar los controles inmediatamente
+                videoView.start()  // Iniciar el video
+                videoView.pause()  // Pausar inmediatamente para que el usuario pueda iniciar cuando quiera
             }
         }
     }
 
     private fun addMediaControllerToContainer() {
         controllerContainer.removeAllViews()
-        mediaController.visibility = View.VISIBLE
 
         // Remover el MediaController de su padre actual si lo tiene
         val parent = mediaController.parent as? ViewGroup
